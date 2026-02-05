caf364bc1228dd4ca96487d4c9a5cea2e004bfdc
vishal kamdi <vishalkamdi2901@gmail.com>
2026-02-05 20:07:20 +0530

    change config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/geo-data-dashboard/", // 👈 repo name
+  base: "/geo-data-dashboard/", //
 });
